cf38fb8428b47ecb67f80e631522cb9bdca75a3a
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-05-17 00:21:20 -0700

    Update hash_collision.py

--- hash_collision.py
+++ hash_collision.py
@@ -5,8 +5,6 @@ import string
 
 
 def hash_collision(k):
-
-    print(k)
     if not isinstance(k, int):
         print("hash_collision expects an integer")
         return b'\x00', b'\x00'
@@ -19,13 +17,13 @@ def hash_collision(k):
     letters = string.ascii_letters
     x = (''.join(random.choice(letters) for i in range(random.randint(1, 10)))).encode('utf-8')
     hash_x = hashlib.sha256(x).hexdigest()
-    
-    print(x)
+    comp_x = hash_x[-k:]
 
     while True:
         y = (''.join(random.choice(letters) for i in range(random.randint(1, 10)))).encode('utf-8')
         hash_y = hashlib.sha256(y).hexdigest()
-        if hash_x[-k:] == hash_y[-k:] and x != y:
+        comp_y = hash_y[-k:]
+        if comp_x == comp_y and x != y:
             break
 
     return x, y
